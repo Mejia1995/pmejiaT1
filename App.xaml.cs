b655e8a7b3931337b0258841851c4d6018c9c5ec
Pedro Mejia <Pedro Mejia@DESKTOP-A88EQLO>
2024-04-14 09:44:29 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace pmejiaT1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace pmejiaT1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculo_Clicked(object sender, EventArgs e)
+        {
+            string nombre=txtNombre.Text;
+            string apellido=txtApellido.Text;
+            int edad= Convert.ToInt32(txtEdad.Text);
+            double salario = Convert.ToDouble(txtSalario.Text);
+            double aporte = 9.45 * salario/100;
+
+
+            DisplayAlert("Alerta", " Bienvenido: " +nombre +" "+ apellido+ "\n Tienes: " +edad+ " años " + "\n Tu aporte mensual es: " +aporte+ " Dolares ", "Cerrar");
+
+        }
+    }
+
+}
